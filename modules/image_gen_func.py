e843b8f72eed20f39f4660e73aa19b999b67634b
Xin Zhao <xin@glowing.com>
2023-05-05 10:29:09 +0800

    feat: an image generation server, and corresponding pkgs and func.

---
+++ modules/image_gen_func.py
@@ -0,0 +1,30 @@
+import logging
+from urllib.parse import quote_plus
+
+import colorama
+import openai
+
+
+def gen_image_from_prompt(
+    prompt: str,
+    image_size: str,
+    n: int
+):
+    logging.info(f"gen_image:" + colorama.Fore.BLUE +
+                 f"prompt={prompt!r}, size={image_size!r}, n={n!r}" +
+                 colorama.Style.RESET_ALL)
+
+    response = openai.Image.create(
+        prompt=prompt,
+        n=n,
+        size=image_size
+    )
+
+    logging.info(f"gen_image:" + colorama.Fore.GREEN +
+                 f"resp={response}" + colorama.Style.RESET_ALL)
+
+    result_urls = []
+    for i in response['data']:
+        result_urls.append(i['url'])
+
+    return result_urls
